dd0ab61483a14568ccac73b0d315e2d8b5838534
BuildTools <unconfigured@null.spigotmc.org>
2020-01-09 16:45:05 +0100

    init

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -19,12 +20,27 @@ public class Main {
 
 	private MainWindow frame;
 	private JTextArea consoleOut;
+	private ArrayList<String> folderBlacklist = new ArrayList<>();
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+
 			public void run() {
 				try {
 					main = new Main();
+					main.folderBlacklist.add("assets");
+					main.folderBlacklist.add("bin");
+					main.folderBlacklist.add("crash-reports");
+					main.folderBlacklist.add("libraries");
+					main.folderBlacklist.add("resourcepacks");
+					main.folderBlacklist.add("resources");
+					main.folderBlacklist.add("saves");
+					main.folderBlacklist.add("screenshots");
+					main.folderBlacklist.add("server-resource-packs");
+					main.folderBlacklist.add("stats");
+					main.folderBlacklist.add("texturepacks");
+					main.folderBlacklist.add("texturepacks-mp-cache");
+					main.folderBlacklist.add("versions");
 					main.frame = new MainWindow(main);
 					main.frame.setVisible(true);
 				} catch (Exception e) {
@@ -35,9 +51,87 @@ public class Main {
 	}
 
 	public void analyze(String filePath, JTextArea consoleOut) {
-		// TODO Auto-generated method stub
 		this.consoleOut = consoleOut;
-		consoleOut("File path:" + filePath + "\n");
+		// check if the file exists
+		File test = new File(filePath);
+		if (!test.exists()) {
+			consoleOut("[ Failure ]  File or folder doesn't exist\n");
+			return;
+		}
+		if (test.isDirectory())
+			listFilesForFolder(test, 0);
+		if (test.isFile())
+			getLinesFiltered(filePath).stream().forEach(a -> consoleOut(a + "\n"));
+	}
+
+	private List<String> getLinesFiltered(String filePath) {
+		// unzip the file
+		// https://stackoverflow.com/questions/1080381/gzipinputstream-reading-line-by-line
+		GZIPInputStream inputFile;
+		try {
+			inputFile = new GZIPInputStream(new FileInputStream(filePath));
+			Reader decoder = new InputStreamReader(inputFile, "UTF-8");
+			// read the unzipped file
+			BufferedReader br = new BufferedReader(decoder);
+			List<String> result = br.lines().filter(a -> a.contains("[CHAT]"))
+					.filter(a -> a.contains(main.frame.getLineFilter())).map(a -> getTimeAndDate(filePath, a))
+					.map(a -> removeExcludedChars(a.replaceAll(" \\[Client thread/INFO\\]: \\[CHAT\\] ", "")))
+					.collect(Collectors.toList());
+			br.close();
+			return result;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String getTimeAndDate(String filePath, String a) {
+		if (main.frame.getTimeAndDate())
+			return new File(filePath).getName().substring(0, 10).replaceAll("-", ".") + " "
+					+ a.replaceFirst("\\[", "").replaceFirst("\\]", ";");
+		else
+			return a;
+	}
+
+	private String removeExcludedChars(String input) {
+		if (main.frame.getTimeAndDate())
+			return input.substring(0, 20) + " " + input.substring(20).replaceAll(main.frame.getExcludedChars(), "");
+		else
+			return input.split("]", 2)[1].replaceAll(main.frame.getExcludedChars(), "");
+	}
+
+	private void listFilesForFolder(final File folder, int mode) {
+		if (folder == null)
+			return;
+		if (!folder.exists()) {
+			consoleOut("[ Failure ]  Folder doesn't exist\n");
+			return;
+		}
+		if (folder.isFile()) {
+			if (folder.getName().contains(".log.gz")) {
+				analyze(folder.getAbsolutePath(), consoleOut);
+			}
+		} else if (folder.isDirectory()) {
+			for (final File fileEntry : folder.listFiles()) {
+				if (fileEntry.isDirectory()) {
+					if (!folderBlacklist.contains(fileEntry.getName()))
+						listFilesForFolder(fileEntry, mode);
+				} else {
+					if (fileEntry.getName().contains(".log.gz")) {
+						if (mode == 0)
+							analyze(fileEntry.getAbsolutePath(), consoleOut);
+						else if (mode == 1)
+							analyzeEyes(fileEntry.getAbsolutePath(), consoleOut);
+					}
+				}
+			}
+		}
+	}
+
+	public void analyzeEyes(String filePath, JTextArea consoleOut) {
+		this.consoleOut = consoleOut;
 		try {
 			// check if the file exists
 			File test = new File(filePath);
@@ -45,74 +139,84 @@ public class Main {
 				consoleOut("[ Failure ]  File doesn't exist\n");
 				return;
 			}
-			// unzip the file
-			// https://stackoverflow.com/questions/1080381/gzipinputstream-reading-line-by-line
-			GZIPInputStream inputFile = new GZIPInputStream(new FileInputStream(filePath));
-			Reader decoder = new InputStreamReader(inputFile, "UTF-8");
-			// read the unzipped file
-			BufferedReader br = new BufferedReader(decoder);
-			List<PlacedEye> placed = br.lines().filter(a -> a.contains("placed a Summoning Eye!"))
-					.map(a -> a.replace(" Brace yourselves!", ""))
-					.map(a -> new PlacedEye(a.split(" ")[5],
-							Integer.parseInt("" + a.split(" ")[10].split("/")[0].charAt(1))))
-					.collect(Collectors.toList());
-			Spawning spawning = new Spawning();
-			List<Spawning> spawnings = new ArrayList<>();
-			for (PlacedEye e : placed) {
-				switch (e.getNumber()) {
-				case 1:
-					spawning = new Spawning();
-					spawning.add(e);
-					break;
-				case 8:
-					spawning.add(e);
-					spawnings.add(spawning);
-					spawning.calcPlacingProbabilities();
-					spawning = new Spawning();
-					break;
-				default:
-					spawning.add(e);
-					break;
-				}
-			}
-			List<String> names = placed.stream().map(a -> a.getName()).distinct().collect(Collectors.toList());
-			List<PlacedInTotal> result = new ArrayList<>();
-			int sure = 0;
-			int maybe = 0;
-			for (String n : names) {
-				int c = 0;
-				int m = 0;
+			if (test.isDirectory())
+				listFilesForFolder(test, 1);
+			if (test.isFile()) {
+				// unzip the file
+				// https://stackoverflow.com/questions/1080381/gzipinputstream-reading-line-by-line
+				GZIPInputStream inputFile = new GZIPInputStream(new FileInputStream(filePath));
+				Reader decoder = new InputStreamReader(inputFile, "UTF-8");
+				// read the unzipped file
+				BufferedReader br = new BufferedReader(decoder);
+				List<PlacedEye> placed = br.lines().filter(a -> a.contains("placed a Summoning Eye!"))
+						.map(a -> a.replace(" Brace yourselves!", ""))
+						.map(a -> new PlacedEye(a.split(" ")[5],
+								Integer.parseInt("" + a.split(" ")[10].split("/")[0].charAt(1))))
+						.collect(Collectors.toList());
+				Spawning spawning = new Spawning();
+				List<Spawning> spawnings = new ArrayList<>();
 				for (PlacedEye e : placed) {
-					if (e.getName().equalsIgnoreCase(n)) {
-						double p = e.getProbability();
-						if (p >= 1.0) {
-							c += 1;
-							m += (int) p - 1;
-						} else if (p == 0.0)
-							;
-						else
-							m += Math.max(1, (int) p - 1);
+					switch (e.getNumber()) {
+					case 1:
+						spawning = new Spawning();
+						spawning.add(e);
+						break;
+					case 8:
+						spawning.add(e);
+						spawnings.add(spawning);
+						spawning.calcPlacingProbabilities();
+						spawning = new Spawning();
+						break;
+					default:
+						spawning.add(e);
+						break;
 					}
 				}
-				if (c + m > 0) {
-					result.add(new PlacedInTotal(n, c, m));
-					sure += c;
-					maybe += m;
+				List<String> names = placed.stream().map(a -> a.getName()).distinct().collect(Collectors.toList());
+				List<PlacedInTotal> result = new ArrayList<>();
+				int sure = 0;
+				int maybe = 0;
+				for (String n : names) {
+					int c = 0;
+					int m = 0;
+					for (PlacedEye e : placed) {
+						if (e.getName().equalsIgnoreCase(n)) {
+							double p = e.getProbability();
+							if (p >= 1.0) {
+								c += 1;
+								m += (int) p - 1;
+							} else if (p == 0.0)
+								;
+							else
+								m += Math.max(1, (int) p - 1);
+						}
+					}
+					if (c + m > 0) {
+						result.add(new PlacedInTotal(n, c, m));
+						sure += c;
+						maybe += m;
+					}
 				}
+
+				if (result.isEmpty()) {
+					br.close();
+					return;
+				}
+				consoleOut("File path:" + filePath + "\n");
+				if (!frame.getChckbx_ResultsOnly().isSelected())
+					placed.stream().forEach(
+							a -> consoleOut(a.getName() + ": " + a.getNumber() + ", " + a.getProbability() + "\n"));
+				// consoleOut("\n");
+				consoleOut("Result:\n");
+				result.stream().sorted().forEach(a -> consoleOut(a + "\n"));
+				// consoleOut("\n");
+				consoleOut("Final result:\n");
+				consoleOut(sure + " placed for sure (= " + (sure / 8.0) + " dragons)\n" + "unsure about " + maybe
+						+ " more (= " + ((sure + maybe) / 8.0) + " dragons)\n");
+				consoleOut("In the end there were " + (spawnings.size() * 8) + " eyes placed (= " + spawnings.size()
+						+ " dragons)\n");
+				br.close();
 			}
-			if (!frame.getChckbx_ResultsOnly().isSelected())
-				placed.stream().forEach(
-						a -> consoleOut(a.getName() + ": " + a.getNumber() + ", " + a.getProbability() + "\n"));
-			consoleOut("\n");
-			consoleOut("Result:\n");
-			result.stream().sorted().forEach(a -> consoleOut(a + "\n"));
-			consoleOut("\n");
-			consoleOut("Final result:\n");
-			consoleOut(sure + " placed for sure (= " + (sure / 8.0) + " dragons)\n" + "unsure about " + maybe
-					+ " more (= " + ((sure + maybe) / 8.0) + " dragons)\n");
-			consoleOut("In the end there were " + (spawnings.size() * 8) + " eyes placed (= " + spawnings.size()
-					+ " dragons)\n");
-			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			for (StackTraceElement s : e.getStackTrace())
@@ -123,5 +227,7 @@ public class Main {
 
 	private static void consoleOut(String s) {
 		main.consoleOut.append(s);
+		if (s.contains("\n"))
+			main.frame.incWrittenLines();
 	}
 }
--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -13,6 +13,8 @@ import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.List;
 
@@ -21,6 +23,7 @@ import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.JScrollPane;
 import javax.swing.JCheckBox;
+import java.awt.FlowLayout;
 
 public class MainWindow extends JFrame {
 
@@ -32,6 +35,10 @@ public class MainWindow extends JFrame {
 	private String filePath;
 	private JTextArea consoleOut;
 	private JCheckBox chckbx_ResultsOnly;
+	private JTextArea lineFilter_textField;
+	private JTextArea chckbxOnlyNumbers;
+	private JCheckBox chckbx_Timestamp;
+	private JLabel label_writtenLines;
 
 	public JCheckBox getChckbx_ResultsOnly() {
 		return chckbx_ResultsOnly;
@@ -47,7 +54,7 @@ public class MainWindow extends JFrame {
 	 */
 	public MainWindow(Main main) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 690, 529);
+		setBounds(100, 100, 779, 529);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -59,17 +66,19 @@ public class MainWindow extends JFrame {
 
 		JPanel panel = new JPanel();
 		splitPane.setLeftComponent(panel);
-		panel.setLayout(new GridLayout(3, 1, 0, 0));
+		panel.setLayout(new GridLayout(5, 1, 2, 2));
 
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1);
-		panel_1.setLayout(new BorderLayout(0, 0));
+		panel_1.setLayout(new BorderLayout(3, 3));
 
-		JLabel lblLogFilePath = new JLabel("Log File Path (drag'n'drop .log.gz-file here):");
+		JLabel lblLogFilePath = new JLabel("Log file path (drag'n'drop .log.gz-file here):");
 		lblLogFilePath.setHorizontalAlignment(SwingConstants.LEFT);
 		panel_1.add(lblLogFilePath, BorderLayout.WEST);
 
 		JTextArea filePath_textField = new JTextArea();
+		filePath_textField.setText("C:\\Users\\User\\AppData\\Roaming\\.minecraft");
+		filePath = filePath_textField.getText();
 		filePath_textField.setLineWrap(true);
 		// https://stackoverflow.com/questions/9669530/drag-and-drop-file-path-to-java-swing-jtextfield
 		filePath_textField.setDropTarget(new DropTarget() {
@@ -93,36 +102,129 @@ public class MainWindow extends JFrame {
 				}
 			}
 		});
+		filePath_textField.addKeyListener(new KeyListener() {
 
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				filePath = filePath_textField.getText();
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+			}
+		});
 		panel_1.add(filePath_textField, BorderLayout.CENTER);
+
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2);
+		panel_2.setLayout(new BorderLayout(3, 3));
+
+		JLabel lblFilter = new JLabel("Filter for containing / Exclude characters with a regex:");
+		lblFilter.setHorizontalAlignment(SwingConstants.LEFT);
+		panel_2.add(lblFilter, BorderLayout.WEST);
+
+		lineFilter_textField = new JTextArea();
+		lineFilter_textField.setText(" coins as interest!");
+		lineFilter_textField.setLineWrap(true);
+		panel_2.add(lineFilter_textField, BorderLayout.CENTER);
+
+		chckbxOnlyNumbers = new JTextArea();
+		chckbxOnlyNumbers.setText("[^0-9]");
+		lineFilter_textField.setLineWrap(true);
+		panel_2.add(chckbxOnlyNumbers, BorderLayout.EAST);
+		chckbxOnlyNumbers.setColumns(10);
 
 		JPanel panel_3 = new JPanel();
 		panel.add(panel_3);
 
-		JButton btnAnalyze = new JButton("Analyze");
+		JButton btnAnalyze = new JButton("Filter");
 		btnAnalyze.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				consoleOut.setText("");
+				resetWrittenLines();
 				main.analyze(getFilePath(), consoleOut);
 			}
 		});
 		panel_3.add(btnAnalyze);
-		
+
+		chckbx_Timestamp = new JCheckBox("Timestamp");
+		chckbx_Timestamp.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(chckbx_Timestamp);
+
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4);
+
+		JButton btnEyeanalyzer = new JButton("Sum Eye Analyzer");
+		btnEyeanalyzer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				consoleOut.setText("");
+				resetWrittenLines();
+				main.analyzeEyes(getFilePath(), consoleOut);
+			}
+		});
+		panel_4.add(btnEyeanalyzer);
+
 		chckbx_ResultsOnly = new JCheckBox("only show the final results");
+		panel_4.add(chckbx_ResultsOnly);
 		chckbx_ResultsOnly.setSelected(true);
-		panel.add(chckbx_ResultsOnly);
 		chckbx_ResultsOnly.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
+		JPanel panel_5 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_5.getLayout();
+		flowLayout.setVgap(10);
+		panel.add(panel_5);
+
+		JLabel lblLinesWritten = new JLabel("Lines written: ");
+		panel_5.add(lblLinesWritten);
+
+		label_writtenLines = new JLabel("0");
+		panel_5.add(label_writtenLines);
+
 		JScrollPane scrollPane = new JScrollPane();
 		splitPane.setRightComponent(scrollPane);
-		
-				consoleOut = new JTextArea();
-				scrollPane.setViewportView(consoleOut);
+
+		consoleOut = new JTextArea();
+		scrollPane.setViewportView(consoleOut);
+	}
+
+	private int getWrittenLines() {
+		//TODO parsing is ugly and slow. replace by integer variable later
+		return Integer.parseInt(label_writtenLines.getText());
+	}
+
+	private void setWrittenLines(int writtenLines) {
+		this.label_writtenLines.setText("" + writtenLines);
+		// this.label_writtenLines.paint(label_writtenLines.getGraphics());
+	}
+	
+	public void incWrittenLines() {
+		setWrittenLines(getWrittenLines() + 1);
+	}
+	public void resetWrittenLines() {
+		setWrittenLines(0);
+	}
+
+	public String getExcludedChars() {
+		return chckbxOnlyNumbers.getText();
+	}
+
+	public String getLineFilter() {
+		return lineFilter_textField.getText();
 	}
 
 	public String getFilePath() {
 		return filePath;
+	}
+
+	public boolean getTimeAndDate() {
+		return chckbx_Timestamp.isSelected();
 	}
 
 }
